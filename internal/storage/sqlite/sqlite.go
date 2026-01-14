d2d8a57a5f7efafc7a71b2c2ad6bfcdcf00e5e73
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-01-14 16:49:48 +0530

    add get student by id

--- internal/storage/sqlite/sqlite.go
+++ internal/storage/sqlite/sqlite.go
@@ -2,8 +2,10 @@ package sqlite
 
 import (
 	"database/sql"
+	"fmt"
 
 	"github.com/Raghunandan-79/students-api/internal/config"
+	"github.com/Raghunandan-79/students-api/internal/types"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -57,3 +59,27 @@ func (s *Sqlite) CreateStudent(name string, email string, age int) (int64, error
 
 	return lastId, nil
 }
+
+func (s *Sqlite) GetStudentById(id int64) (types.Student, error) {
+	stmt, err := s.Db.Prepare("SELECT id, name, email, age FROM students WHERE id = ? LIMIT 1")
+	
+	if err != nil {
+		return types.Student{}, err
+	}
+
+	defer stmt.Close()
+
+	var student types.Student
+
+	err = stmt.QueryRow(id).Scan(&student.Id, &student.Name, &student.Email, &student.Age)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return types.Student{}, fmt.Errorf("No student found with id %s", fmt.Sprint(id))
+		}
+
+		return types.Student{}, fmt.Errorf("query errror: %w", err)
+	}
+
+	return student, nil
+} 
